f6c36f7b87c7b6b646bd1c641d84b67962439dc8
Zack <zack105@vt.edu>
2017-04-08 17:52:15 -0400

    adding service class for Drink

--- Sippable/src/main/java/com/sippable/test/App.java
+++ Sippable/src/main/java/com/sippable/test/App.java
@@ -36,60 +36,20 @@ public class App
 //        System.out.println(user);
     	
     		DrinkDaoImpl dao = new DrinkDaoImpl();
-//    		Drink dr = dao.getDrink(100000);
+    		Drink dr = dao.getDrink(100000);
+    		dr.setDrinkName("Beer");
 //    		System.out.println(dr.toString());
     		//dao.addDrink("Bud Light", 1, "college", "sub par", "yellow", "Anwiester Bush", 4.5, 30, 1);
-    		List<Drink> li = dao.getAllDrinks();
-    		for(Drink d : li){
-    			System.out.println(d.toString());
-    		}
+//    		List<Drink> li = dao.getAllDrinks();
+//    		for(Drink d : li){
+//    			System.out.println(d.toString());
+//    		}
     		
 //    		System.out.println("done");
         
         
         
         
-//        sess.close();
-//        HoneyPot hp = (HoneyPot)sess.get(HoneyPot.class, 50);
-//       Cave c = (Cave)sess.get(Cave.class, 1100);
-        //Bear b = (Bear)sess.get(Bear.class, 1550);
-        
-        //bears with id > 1000
-//        System.out.println("all Bears with an id > 1000\n-------------------------------------------------------");
-//        Criteria cr = sess.createCriteria(Bear.class);
-//        cr.add(Restrictions.gt("bearId", 1000));
-//        
-//        List<Bear> rs = cr.list();
-//        for(Bear b : rs){
-//        	System.out.println(b.toString());
-//        }
-////       
-//        System.out.println("\n\nPrints the bear with id of 1550\n---------------------------------------------");
-////        //selects bear with id of 1550
-//        String hql = "FROM Bear where BEAR_ID = :x";
-//        Query q = sess.createQuery(hql);
-//        q.setParameter("x", 1550);
-//        System.out.println(q.list());
-//        
-//        
-//        System.out.println("\n\n Prints all the bears running a named query\n----------------------------------");
-//        //running a named query called nQ which will print all the bears
-//        Query query = sess.getNamedQuery("nQ");
-//        List<Bear> l = query.list();
-//        for(Bear b : l){
-//        	System.out.println(b.toString());
-//        }
-//        
-//        
-//        
-//        System.out.println("\n\nNative SQL query to select all Bears\n-----------------------------------");
-//        SQLQuery s = sess.createSQLQuery("SELECT * FROM BEAR").addEntity(Bear.class);
-//        List<Bear> li = s.list();
-//        for(Bear b : li){
-//        	System.out.println(b.toString());
-//        }
-//        
-//        sess.close();
-       // System.out.println("done");
+//      
     }
 }
