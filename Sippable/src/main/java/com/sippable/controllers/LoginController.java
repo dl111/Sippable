6924a121ed615db96cbb15994c1edfa4e20628fc
dl111 <david.lund111@gmail.com>
2017-04-09 15:43:30 -0400

    pages added

---
+++ Sippable/src/main/java/com/sippable/controllers/LoginController.java
@@ -0,0 +1,27 @@
+package com.sippable.controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.sippable.*;
+import com.sippable.beans.Users;
+
+
+@Controller
+@RequestMapping(value = "/login")
+public class LoginController {
+	
+	@Autowired
+	Users emptyUser;
+	
+
+	
+}
