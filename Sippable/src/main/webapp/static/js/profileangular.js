f77b92ed03d35dcc00339193d2c1a0f8a2e90bd0
Edward Young <youngedward94@gmail.com>
2017-04-14 23:52:52 -0400

    changed paths

--- Sippable/src/main/webapp/static/js/profileangular.js
+++ Sippable/src/main/webapp/static/js/profileangular.js
@@ -10,7 +10,7 @@ app.controller("initValCtrl", function($scope, $http){
 	
 	$http({
 		  method: 'GET',
-		  url: '34.200.225.32/Sippables/profileinit'
+		  url: '/profileinit'
 		}).then(function successCallback(response) {
 			console.log(response);
 		    $scope.user = response.data;
@@ -23,7 +23,7 @@ app.controller("initValCtrl", function($scope, $http){
 		var newfname = $scope.user.newfname;
 		$http({
 			  method: 'POST',
-			  url: '34.200.225.32/Sippables/profilechangefname',
+			  url: '/profilechangefname',
 			  data: newfname
 			}).then(function successCallback(response) {
 				console.log(response);
@@ -31,7 +31,7 @@ app.controller("initValCtrl", function($scope, $http){
 			    
 			    $http({
 					  method: 'GET',
-					  url: 'http://localhost:8090/Sippables/profileinit'
+					  url: '/profileinit'
 					}).then(function successCallback(response) {
 						console.log(response);
 					    $scope.user = response.data;
@@ -49,7 +49,7 @@ app.controller("initValCtrl", function($scope, $http){
 		var newlname = $scope.user.newlname;
 		$http({
 			  method: 'POST',
-			  url: '34.200.225.32/Sippables/profilechangelname',
+			  url: '/profilechangelname',
 			  data: newlname
 			}).then(function successCallback(response) {
 				console.log(response);
@@ -57,7 +57,7 @@ app.controller("initValCtrl", function($scope, $http){
 			    
 			    $http({
 					  method: 'GET',
-					  url: '34.200.225.32/Sippables/profileinit'
+					  url: '/profileinit'
 					}).then(function successCallback(response) {
 						console.log(response);
 					    $scope.user = response.data;
@@ -76,7 +76,7 @@ app.controller("initValCtrl", function($scope, $http){
 		var newemail = $scope.user.newemail;
 		$http({
 			  method: 'POST',
-			  url: '34.200.225.32/Sippables/profilechangeemail',
+			  url: '/profilechangeemail',
 			  data: newemail
 			}).then(function successCallback(response) {
 				console.log(response);
@@ -84,7 +84,7 @@ app.controller("initValCtrl", function($scope, $http){
 			    
 			    $http({
 					  method: 'GET',
-					  url: 'http://localhost:8090/Sippables/profileinit'
+					  url: '/profileinit'
 					}).then(function successCallback(response) {
 						console.log(response);
 					    $scope.user = response.data;
@@ -103,7 +103,7 @@ app.controller("initValCtrl", function($scope, $http){
 		var newpassword = $scope.user.newpw;
 		$http({
 			  method: 'POST',
-			  url: '34.200.225.32/Sippables/profilechangepw',
+			  url: '/profilechangepw',
 			  data: newpassword
 			}).then(function successCallback(response) {
 				console.log(response);
@@ -111,7 +111,7 @@ app.controller("initValCtrl", function($scope, $http){
 			    
 			    $http({
 					  method: 'GET',
-					  url: '34.200.225.32/Sippables/profileinit'
+					  url: '/profileinit'
 					}).then(function successCallback(response) {
 						console.log(response);
 					    $scope.user = response.data;
